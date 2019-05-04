7c3ef10bebbfd4250a43635caa42768a20624174
Juker1141 <juker1141@gmail.com>
2019-05-04 21:42:32 +0800

    Update avg reviews

--- comment.py
+++ comment.py
@@ -8,3 +8,7 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 
 print('檔案讀取完畢, 總共有', len(data), '筆留言')
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('留言的平均長度為', sum_len / len(data), '個字')
